097a44aa88fe79373ca28e372563bff36f0bfe40
NipuniGitAccount <nipunimadushani52@gmail.com>
2019-06-17 22:05:44 +0530

    saved customer successfully and check all crud operation using postman

--- src/CorsFilter.java
+++ src/CorsFilter.java
@@ -10,8 +10,13 @@ import java.io.IOException;
 public class CorsFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        response.setHeader("Access-Control-Allow-Origin","http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Methods","OPTIONS, GET");
+        response.setHeader("Access-Control-Allow-Origin","http://localhost:4300");
+        response.setHeader("Access-Control-Allow-Methods","OPTIONS, GET,POST");
+        response.setHeader("Access-Control-Allow-Headers","Content-Type");
+
+//        response.setHeader("Content-Type", "application/json");
+//        response.setHeader("Accept", "application/json");
         super.doFilter(request, response, chain);
     }
 }
+
